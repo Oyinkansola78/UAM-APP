f3e12e21370d19235a8429443dd989dfbc62dad6
Oyinkansola78 <oyinkanayannuga@example.com>
2025-06-24 12:04:25 +0100

    backend-connected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gtco-uam-sub-system.vercel.app/dashboard',
-  supabaseUrl: 'https://ggdtndeklnjyhfbvlthm.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnZHRuZGVrbG5qeWhmYnZsdGhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2OTI5NzIsImV4cCI6MjA2NjI2ODk3Mn0.0eXJTDKyEZDplNGnLDyCWq4esl-6mk3slopgSbjl-Y4'
+  apiUrl: 'https://uam-backend-production.up.railway.app/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://uam-backend-production.up.railway.app/'
 };
